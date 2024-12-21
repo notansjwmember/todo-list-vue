9672c24f67eadcec6bec31f2fc5d835705650504
caenar <73683836+notansjwmember@users.noreply.github.com>
2024-12-21 23:25:42 +0800

    Added fetch one, update and delete for todos

--- backend/src/todos/todos.controller.ts
+++ backend/src/todos/todos.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './todos.model';
 
 @Controller('todos')
 export class TodosController {
@@ -20,6 +29,49 @@ export class TodosController {
     try {
       const todos = await this.todosService.fetchAll();
       return { data: todos };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+
+  @Get(':id')
+  async fetchOne(@Param('id') id: number) {
+    try {
+      const todo = await this.todosService.fetchOne(id);
+
+      return { data: todo };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: { title: string; content: string; completed?: boolean },
+  ) {
+    try {
+      const updates: Todo = {
+        id,
+        title: body.title,
+        content: body.content,
+        completed: body.completed ?? false,
+      };
+
+      const updatedTodo = await this.todosService.update(id, updates);
+
+      return { data: updatedTodo };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    try {
+      const deletedTodo = await this.todosService.delete(id);
+
+      return { data: deletedTodo };
     } catch (error) {
       return { error: error.message };
     }
--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { SupabaseService } from 'src/supabase/supabase.service';
+import { Todo } from './todos.model';
 
 @Injectable()
 export class TodosService {
@@ -37,6 +38,58 @@ export class TodosService {
     }
 
     this.logger.log('Succesfully fetched todos');
+    return data;
+  }
+
+  async fetchOne(id: number) {
+    this.logger.log(`Fetching todo with id of ${id}`);
+
+    const { data, error } = await this.supabaseService
+      .getClient()
+      .from('todos')
+      .select('*')
+      .eq('id', id)
+      .single();
+    if (error) {
+      this.logger.error(`Error fetching todo with id ${id}: ${error.message}`);
+      throw new Error(`Error fetching todo: ${error.message}`);
+    }
+
+    this.logger.log(`Successfully fetched todo with id ${id}`);
+    return data;
+  }
+
+  async update(id: number, updates: Partial<Todo>) {
+    this.logger.log(`Fetching todo with id of ${id}`);
+
+    const { data, error } = await this.supabaseService
+      .getClient()
+      .from('todos')
+      .update(updates)
+      .eq('id', id);
+    if (error) {
+      this.logger.error(`Error updating todo with ${id}`);
+      throw new Error(`Error updating todo: ${error.message}`);
+    }
+
+    this.logger.log(`Succesfully updated todo with id of ${id}`);
+    return data;
+  }
+
+  async delete(id: number) {
+    this.logger.log(`Deleting todo with id of ${id}`);
+
+    const { data, error } = await this.supabaseService
+      .getClient()
+      .from('todos')
+      .delete()
+      .eq('id', id);
+    if (error) {
+      this.logger.error(`Error deleting todo with id ${id}: ${error.message}`);
+      throw new Error(`Error deleting todo: ${error.message}`);
+    }
+
+    this.logger.log(`Successfully deleted todo with id ${id}`);
     return data;
   }
 }
